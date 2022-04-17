b44e78b67efa148370a5bef79aa23bb9121012ea
ysaad01 <ysaad2325@gmail.com>
2022-04-17 14:01:56 -0400

    added generatePortfolio.js file and index.js file to invoke application

--- index.js
+++ index.js
@@ -1,8 +1,270 @@
+/* imports */
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 const generatePortfolio = require('./src/generatePortfolio');
+
+team = [];
+
+const managerQs = () => {
+  inquirer
+    .prompt([
+      {
+        message: "What is the Manager's name?",
+        type: "input",
+        name: "name",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            return "Please enter the Manager's name.";
+          }
+        }
+      },
+
+      {
+        message: "What is the Manager's ID number?",
+        type: "input",
+        name: "id",
+        validate: idInput => {
+          if (isNaN(idInput) || idInput === "") {
+            return "Please enter a valid ID.";
+          } else {
+            return true;
+          }
+        }
+      },
+
+      {
+        message: "What is the Manager's email address?",
+        type: "input",
+        name: "email",
+        validate: emailInput => {
+          if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(emailInput)) {
+            return true;
+          } else {
+            return "Please enter a valid email address.";
+          }
+        }
+      },
+
+      {
+        message: "What is the Manager's office number?",
+        type: "input",
+        name: "office",
+        validate: officeInput => {
+          if (isNaN(officeInput) || officeInput === "") {
+            return "Please enter a valid office number.";
+          } else {
+            return true;
+          }
+        }
+      },
+
+      {
+        message: "What type of Team Member would you like to add next:",
+        type: "list",
+        name: "addTM",
+        choices: ["Engineer", "Intern", "No more Team Members to add"],
+      }
+    ])
+    .then((managerAs) => {
+      const manager = new Manager(
+        managerAs.id,
+        managerAs.name,
+        managerAs.email,
+        managerAs.office
+      )
+      team.push(manager)
+      switch (managerAs.addTM) {
+        case "Engineer":
+          engineerQs();
+          break;
+        case "Intern":
+          internQs();
+          break;
+        default:
+          writeToFile("dist/index.html", generatePortfolio(team))
+      }
+    });
+}
+
+const engineerQs = () => {
+  inquirer
+    .prompt([
+      {
+        message: "What is the Engineer's name?",
+        type: "input",
+        name: "name",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            return "Please enter the Engineer's name.";
+          }
+        }
+      },
+
+      {
+        message: "What is the Engineer's ID number?",
+        type: "input",
+        name: "id",
+        validate: idInput => {
+          if (isNaN(idInput) || idInput === "") {
+            return "Please enter a valid ID.";
+          } else {
+            return true;
+          }
+        }
+      },
+
+      {
+        message: "What is the Engineer's email address?",
+        type: "input",
+        name: "email",
+        validate: emailInput => {
+          if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(emailInput)) {
+            return true;
+          } else {
+            return "Please enter a valid email address.";
+          }
+        }
+      },
+
+      {
+        message: "What is the Engineer's GitHub username?",
+        type: "input",
+        name: "github",
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            return "Please enter GitHub username"
+          }
+        }
+      },
+
+      {
+        message: "What type of Team Member would you like to add next:",
+        type: "list",
+        name: "addTM",
+        choices: ["Engineer", "Intern", "No more Team Members to add"],
+      }
+    ])
+    .then((engineerAs) => {
+      const engineer = new Engineer(
+        engineerAs.id,
+        engineerAs.name,
+        engineerAs.email,
+        engineerAs.github
+      );
+      team.push(engineer);
+      switch (engineerAs.addTM) {
+        case "Engineer":
+          engineerQs();
+          break;
+        case "Intern":
+          internQs();
+          break;
+        default:
+          writeToFile("dist/index.html", generatePortfolio(team));
+      }
+    });
+};
+
+const internQs = () => {
+  inquirer
+    .prompt([
+      {
+        message: "What is the Intern's name?",
+        type: "input",
+        name: "name",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            return "Please enter the Intern's name.";
+          }
+        },
+      },
+
+      {
+        message: "What is the Intern's ID number?",
+        type: "input",
+        name: "id",
+        validate: (idInput) => {
+          if (isNaN(idInput) || idInput === "") {
+            return "Please enter a valid ID.";
+          } else {
+            return true;
+          }
+        },
+      },
+
+      {
+        message: "What is the Intern's email address?",
+        type: "input",
+        name: "email",
+        validate: (emailInput) => {
+          if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(emailInput)) {
+            return true;
+          } else {
+            return "Please enter a valid email address.";
+          }
+        },
+      },
+
+      {
+        message: "What is the Intern's school name?",
+        type: "input",
+        name: "school",
+        validate: (schoolInput) => {
+          if (schoolInput) {
+            return true;
+          } else {
+            return "Please enter the school's name. ";
+          }
+        },
+      },
+
+      {
+        message: "What type of Team Member would you like to add next:",
+        type: "list",
+        name: "addTM",
+        choices: ["Engineer", "Intern", "No more Team Members to add"],
+      },
+    ])
+    .then((internAs) => {
+      const intern = new Intern(
+        internAs.id,
+        internAs.name,
+        internAs.email,
+        internAs.school
+      );
+      team.push(intern);
+      switch (internAs.addTM) {
+        case "Engineer":
+          engineerQs();
+          break;
+        case "Intern":
+          internQs();
+          break;
+        default:
+          writeToFile("dist/index.html", generatePortfolio(team));
+      }
+    });
+};
+
+managerQs()
+
+function writeToFile(filename, data) {
+  fs.writeFile(filename, data, (err) => {
+    if (err) throw err;
+    console.log(`Portfolio saved to ${filename}!`)
+  });
+};
---
+++ src/generatePortfolio.js
@@ -0,0 +1,118 @@
+/* import files */
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+const Manager = require("../lib/Manager");
+
+function generateTeam(team) {
+  let cards = [];
+  for (let i = 0; i < team.length; i++) {
+    const teamArray = team[i];
+    switch (teamArray.getRole()) {
+      case "Manager":
+        const manager = new Manager(
+          teamArray.id,
+          teamArray.name,
+          teamArray.email,
+          teamArray.office
+        );
+        cards.push(managerCard(manager));
+        break;
+      case "Engineer":
+        const engineer = new Engineer(
+          teamArray.id,
+          teamArray.name,
+          teamArray.email,
+          teamArray.github
+        );
+        cards.push(engineerCard(engineer));
+        break;
+      case "Intern":
+        const intern = new Intern(
+          teamArray.id,
+          teamArray.name,
+          teamArray.email,
+          teamArray.school
+        );
+        cards.push(internCard(intern));
+    }
+  }
+  return cards.join(``);
+} 
+
+let managerCard = (manager) => {
+  return `
+    <div class="card h-100 m-2 border-success" style="width:40%;">
+      <div class="card-header">
+        <h3>${manager.getName()}</h3>
+        <h6 class="mb-2 text-muted">${manager.getRole()}</h6>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+        <li class="list-group-item">Office Number: ${manager.getOffice()}</li>
+      </ul>
+    </div>
+  `;
+}
+
+let engineerCard = (engineer) => {
+  return `
+    <div class="card h-100 m-2 border-success" style="width:40%;">
+      <div class="card-header">
+        <h3>${engineer.getName()}</h3>
+        <h6 class="mb-2 text-muted">${engineer.getRole()}</h6>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+        <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+      </ul>
+    </div>
+  `;
+}
+let internCard = (intern) => {
+  return `
+    <div class="card h-100 m-2 border-success" style="width:40%;">
+      <div class="card-header">
+        <h3>${intern.getName()}</h3>
+        <h6 class="mb-2 text-muted">${intern.getRole()}</h6>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item">School: ${intern.getSchool()}</li>
+      </ul>
+    </div>
+  `;
+}
+
+function generatePortfolio(team) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      /* bootstrap link */
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <title>My Team Ü</title>
+    </head>
+
+    <body>
+      <header>
+        <nav class="navbar bg-warning justify-content-center">
+          <h1> My Team Ü </h1>
+        </nav>
+      </header>
+
+      <div class="d-flex flex-row flex-wrap justify-content-center">
+        ${generateTeam(team)}
+      </div>
+
+    </body>
+    </html>
+  `;
+}
+
+module.exports = generatePortfolio; 
